9881c170b1feb13eb823d8c11a166b8cf824310c
Aditya2213 <112240861+Aditya2213@users.noreply.github.com>
2023-08-28 15:46:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    var left = document.querySelector(".scroller");
+    left.scrollBy(922,0)
+  }
+
+  function scrollr() {
+    var right = document.querySelector(".scroller");
+    right.scrollBy(-922,0)
+  }
